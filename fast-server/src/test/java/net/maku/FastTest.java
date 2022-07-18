ac80e6de11943b351260e3508c4cff07bcdb1ac3
阿沐 <babamu@126.com>
2022-07-18 23:53:23 +0800

    新增测试用例DEMO

---
+++ fast-server/src/test/java/net/maku/FastTest.java
@@ -0,0 +1,14 @@
+package net.maku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FastTest {
+
+    @Test
+    public void test(){
+        System.out.println("测试用例！");
+    }
+
+}
