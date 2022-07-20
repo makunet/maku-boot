2258acef4eef43bab070e43dd92c4a6d63e194bd
阿沐 <babamu@126.com>
2022-07-20 12:24:06 +0800

    新增模块，用于开发新业务

---
+++ fast-boot-new/src/main/java/net/maku/module/controller/TestController.java
@@ -0,0 +1,28 @@
+package net.maku.module.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import net.maku.framework.common.utils.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 新模块测试
+ *
+ * @author 阿沐 babamu@126.com
+ */
+@RestController
+@RequestMapping("new/test")
+@Tag(name="新模块测试")
+@AllArgsConstructor
+public class TestController {
+
+    @GetMapping()
+    @Operation(summary = "测试接口")
+    public Result<String> test(){
+
+        return Result.ok("测试数据");
+    }
+}
