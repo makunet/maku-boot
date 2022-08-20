cc5f8cd58da8af958485e0ab75cf2a617db70af8
阿沐 <babamu@126.com>
2022-08-20 16:21:37 +0800

    v1.4.0

--- fast-boot-module/fast-boot-message/src/main/java/net/maku/message/entity/SmsPlatformEntity.java
+++ fast-boot-module/fast-boot-message/src/main/java/net/maku/message/entity/SmsPlatformEntity.java
@@ -11,47 +11,47 @@ import net.maku.framework.common.entity.BaseEntity;
  * @author 阿沐 babamu@126.com
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @TableName("sms_platform")
 public class SmsPlatformEntity extends BaseEntity {
-	/**
-	* 平台类型  0：阿里云   1：腾讯云
-	*/
-	private Integer platform;
+    /**
+     * 平台类型  0：阿里云   1：腾讯云   2：七牛云    3：华为云
+     */
+    private Integer platform;
 
-	/**
-	* 短信签名
-	*/
-	private String signName;
+    /**
+     * 短信签名
+     */
+    private String signName;
 
-	/**
-	* 短信模板
-	*/
-	private String templateId;
+    /**
+     * 短信模板
+     */
+    private String templateId;
 
-	/**
-	 * 短信应用的ID，如：腾讯云等
-	 */
-	private String appId;
+    /**
+     * 短信应用的ID，如：腾讯云等
+     */
+    private String appId;
 
-	/**
-	 * 腾讯云国际短信、华为云等需要
-	 */
-	private String senderId;
+    /**
+     * 腾讯云国际短信、华为云等需要
+     */
+    private String senderId;
 
-	/**
-	* AccessKey
-	*/
-	private String accessKey;
+    /**
+     * AccessKey
+     */
+    private String accessKey;
 
-	/**
-	* SecretKey
-	*/
-	private String secretKey;
+    /**
+     * SecretKey
+     */
+    private String secretKey;
 
-	/**
-	* 状态  0：禁用   1：启用
-	*/
-	private Integer status;
+    /**
+     * 状态  0：禁用   1：启用
+     */
+    private Integer status;
 
 }
--- fast-boot-module/fast-boot-message/src/main/java/net/maku/message/enums/SmsPlatformEnum.java
+++ fast-boot-module/fast-boot-message/src/main/java/net/maku/message/enums/SmsPlatformEnum.java
@@ -11,14 +11,22 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SmsPlatformEnum {
-     /**
-      *  阿里云
+    /**
+     * 阿里云
      */
     ALIYUN(0),
     /**
      * 腾讯云
      */
-    QCLOUD(1);
+    QCLOUD(1),
+    /**
+     * 七牛云
+     */
+    QINIU(2),
+    /**
+     * 华为云
+     */
+    HUAWEI(3);
 
     private final int value;
 
