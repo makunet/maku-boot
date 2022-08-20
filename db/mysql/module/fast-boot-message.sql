521367db709ffe615878504c80088b4de204668b
阿沐 <babamu@126.com>
2022-08-20 16:21:07 +0800

    更新字典数据

--- db/mysql/module/fast-boot-message.sql
+++ db/mysql/module/fast-boot-message.sql
@@ -10,6 +10,14 @@ INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, v
 INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '修改', '', 'sms:platform:update,sms:platform:info', 1, 0, '', 2, 0, 0, 10000, now(), 10000, now());
 INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '删除', '', 'sms:platform:delete', 1, 0, '', 3, 0, 0, 10000, now(), 10000, now());
 
+INSERT INTO sys_dict_type (dict_type, dict_name, remark, sort, version, deleted, creator, create_time, updater, update_time) VALUES ('sms_platform', '平台类型', '短信管理', 0, 0, 0, 10000, now(), 10000, now());
+
+SET @typeId = @@identity;
+INSERT INTO sys_dict_data (dict_type_id, dict_label, dict_value, remark, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @typeId), '阿里云', '0', '', 0, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_dict_data (dict_type_id, dict_label, dict_value, remark, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @typeId), '腾讯云', '1', '', 1, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_dict_data (dict_type_id, dict_label, dict_value, remark, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @typeId), '七牛云', '2', '', 2, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_dict_data (dict_type_id, dict_label, dict_value, remark, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @typeId), '华为云', '3', '', 3, 0, 0, 10000, now(), 10000, now());
+
 
 DROP TABLE IF EXISTS sms_platform;
 DROP TABLE IF EXISTS sms_log;
@@ -17,7 +25,7 @@ DROP TABLE IF EXISTS sms_log;
 CREATE TABLE sms_platform
 (
     id          bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-    platform    tinyint COMMENT '平台类型  0：阿里云   1：腾讯云',
+    platform    tinyint COMMENT '平台类型  0：阿里云   1：腾讯云   2：七牛云   3：华为云',
     sign_name   varchar(100) NOT NULL COMMENT '短信签名',
     template_id varchar(100) NOT NULL COMMENT '短信模板',
     app_id      varchar(100) NOT NULL COMMENT '短信应用ID，如：腾讯云等',
