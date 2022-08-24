303789289e786666efa794b1607bbef29332186c
阿沐 <babamu@126.com>
2022-08-24 11:03:42 +0800

    新增华为云存储

---
+++ fast-boot-system/src/main/java/net/maku/storage/service/HuaweiStorageService.java
@@ -0,0 +1,40 @@
+package net.maku.storage.service;
+
+import com.obs.services.ObsClient;
+import net.maku.framework.common.exception.FastException;
+import net.maku.storage.properties.StorageProperties;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * 华为云存储
+ *
+ * @author 阿沐 babamu@126.com
+ */
+public class HuaweiStorageService extends StorageService {
+
+    public HuaweiStorageService(StorageProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public String upload(byte[] data, String path) {
+        return upload(new ByteArrayInputStream(data), path);
+    }
+
+    @Override
+    public String upload(InputStream inputStream, String path) {
+        ObsClient client = new ObsClient(properties.getHuawei().getAccessKey(),
+                properties.getHuawei().getSecretKey(), properties.getHuawei().getEndPoint());
+        try {
+            client.putObject(properties.getHuawei().getBucketName(), path, inputStream);
+            client.close();
+        } catch (Exception e) {
+            throw new FastException("上传文件失败：", e);
+        }
+
+        return properties.getConfig().getDomain() + "/" + path;
+    }
+
+}
