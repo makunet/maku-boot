b3a347d5c1244bd787f236f1e286d77980d2cc85
阿沐 <babamu@126.com>
2022-08-24 11:04:15 +0800

    新增minio存储

---
+++ fast-boot-system/src/main/java/net/maku/storage/properties/MinioStorageProperties.java
@@ -0,0 +1,16 @@
+package net.maku.storage.properties;
+
+import lombok.Data;
+
+/**
+ * Minio存储配置项
+ *
+ * @author 阿沐 babamu@126.com
+ */
+@Data
+public class MinioStorageProperties {
+    private String endPoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+}
