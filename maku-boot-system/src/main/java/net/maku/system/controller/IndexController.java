b5539b634cc6e50e5deb5556cbe58d1e318d2c95
阿沐 <babamu@126.com>
2022-10-27 14:01:44 +0800

    欢迎信息

---
+++ maku-boot-system/src/main/java/net/maku/system/controller/IndexController.java
@@ -0,0 +1,18 @@
+package net.maku.system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 首页 欢迎信息
+ *
+ * @author 阿沐 babamu@126.com
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "您好，项目已启动，预您使用愉快！";
+    }
+}
