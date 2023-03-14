f7487353be6d793c605405504be421c93b7162d9
阿沐 <babamu@126.com>
2023-03-14 14:08:04 +0800

    格式化

--- maku-boot-module/maku-module-quartz/src/main/java/net/maku/quartz/config/ScheduleConfig.java
+++ maku-boot-module/maku-module-quartz/src/main/java/net/maku/quartz/config/ScheduleConfig.java
@@ -1,10 +1,10 @@
 package net.maku.quartz.config;
 
+import net.maku.framework.common.constant.Constant;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.beans.factory.annotation.Value;
-import net.maku.framework.common.constant.Constant;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -17,7 +17,6 @@ import java.util.Properties;
  */
 @Configuration
 public class ScheduleConfig {
-
     @Value("spring.datasource.driver-class-name")
     private String driver;
 
@@ -43,10 +42,11 @@ public class ScheduleConfig {
         prop.put("org.quartz.jobStore.tablePrefix", "QRTZ_");
         prop.put("org.quartz.jobStore.selectWithLockSQL", "SELECT * FROM {0}LOCKS UPDLOCK WHERE LOCK_NAME = ?");
 
-        //postgreSql数据库配置
-        if(Constant.PGSQL_DRIVER.equals(driver)){
+        // PostgreSQL数据库配置
+        if (Constant.PGSQL_DRIVER.equals(driver)) {
             prop.put("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate");
         }
+
         SchedulerFactoryBean factory = new SchedulerFactoryBean();
         factory.setSchedulerName("MakuScheduler");
         factory.setDataSource(dataSource);
