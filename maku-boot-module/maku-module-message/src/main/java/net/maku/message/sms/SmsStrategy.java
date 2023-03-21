c8f8e6b57d01ab6e36f852748e7beb5e1373f59c
阿沐 <babamu@126.com>
2023-03-21 11:16:38 +0800

    格式化

--- maku-boot-module/maku-module-message/src/main/java/net/maku/message/sms/SmsStrategy.java
+++ maku-boot-module/maku-module-message/src/main/java/net/maku/message/sms/SmsStrategy.java
@@ -12,8 +12,9 @@ public interface SmsStrategy {
 
     /**
      * 发送短信
-     * @param mobile  手机号
-     * @param params  参数
+     *
+     * @param mobile 手机号
+     * @param params 参数
      */
     void send(String mobile, Map<String, String> params);
 }
