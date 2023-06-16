42cf3c687c708285208610cc22f4472e64eb867c
阿沐 <babamu@126.com>
2023-06-16 11:29:00 +0800

    移除未使用的方法

--- maku-boot-system/src/main/java/net/maku/system/cache/SysParamsCache.java
+++ maku-boot-system/src/main/java/net/maku/system/cache/SysParamsCache.java
@@ -1,14 +1,8 @@
 package net.maku.system.cache;
 
-import cn.hutool.core.collection.CollUtil;
 import lombok.AllArgsConstructor;
 import net.maku.framework.common.cache.RedisCache;
-import net.maku.system.entity.SysParamsEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 参数管理 Cache
@@ -27,29 +21,6 @@ public class SysParamsCache {
     private final String SYSTEM_PARAMS_KEY = "system:params";
 
     /**
-     * 保存参数列表到缓存
-     *
-     * @param list 参数列表
-     */
-    public void saveList(List<SysParamsEntity> list) {
-        if (CollUtil.isEmpty(list)) {
-            return;
-        }
-        
-        // list 转成 map
-        Map<String, Object> map = list.stream().collect(Collectors.toMap(SysParamsEntity::getParamKey, SysParamsEntity::getParamValue));
-
-        redisCache.hMSet(SYSTEM_PARAMS_KEY, map, RedisCache.NOT_EXPIRE);
-    }
-
-    /**
-     * 删除缓存中的全部参数列表
-     */
-    public void delList() {
-        redisCache.delete(SYSTEM_PARAMS_KEY);
-    }
-
-    /**
      * 根据参数键，获取参数值
      *
      * @param paramKey 参数键
@@ -65,7 +36,7 @@ public class SysParamsCache {
      * @param paramValue 参数值
      */
     public void save(String paramKey, String paramValue) {
-        redisCache.hSet(SYSTEM_PARAMS_KEY, paramKey, paramValue);
+        redisCache.hSet(SYSTEM_PARAMS_KEY, paramKey, paramValue, RedisCache.NOT_EXPIRE);
     }
 
     /**
