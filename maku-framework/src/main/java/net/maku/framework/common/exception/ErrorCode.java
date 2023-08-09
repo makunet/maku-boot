b59c6c25b763d62329b2daa6087c27460ff19980
阿沐 <babamu@126.com>
2023-08-09 23:24:06 +0800

    新增错误编码

--- maku-framework/src/main/java/net/maku/framework/common/exception/ErrorCode.java
+++ maku-framework/src/main/java/net/maku/framework/common/exception/ErrorCode.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 public enum ErrorCode {
     UNAUTHORIZED(401, "还未授权，不能访问"),
     FORBIDDEN(403, "没有权限，禁止访问"),
+    REFRESH_TOKEN_INVALID(400, "refresh_token 已失效"),
     INTERNAL_SERVER_ERROR(500, "服务器异常，请稍后再试");
 
     private final int code;
