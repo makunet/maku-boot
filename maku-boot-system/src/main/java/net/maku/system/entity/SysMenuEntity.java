a1e799e50b60d0bf912aacf0322462a12e4c2dd4
阿沐 <babamu@126.com>
2023-10-06 15:46:14 +0800

    优化

--- maku-boot-system/src/main/java/net/maku/system/entity/SysMenuEntity.java
+++ maku-boot-system/src/main/java/net/maku/system/entity/SysMenuEntity.java
@@ -12,40 +12,40 @@ import net.maku.framework.mybatis.entity.BaseEntity;
  * <a href="https://maku.net">MAKU</a>
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @TableName("sys_menu")
 public class SysMenuEntity extends BaseEntity {
-	/**
-	 * 上级ID，一级菜单为0
-	 */
-	private Long pid;
-	/**
-	 * 菜单名称
-	 */
-	private String name;
-	/**
-	 * 菜单URL
-	 */
-	private String url;
-	/**
-	 * 授权标识(多个用逗号分隔，如：sys:menu:list,sys:menu:save)
-	 */
-	private String authority;
-	/**
-	 * 类型   0：菜单   1：按钮   2：接口
-	 */
-	private Integer type;
-	/**
-	 * 打开方式   0：内部   1：外部
-	 */
-	private Integer openStyle;
-	/**
-	 * 菜单图标
-	 */
-	private String icon;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
+    /**
+     * 上级ID
+     */
+    private Long pid;
+    /**
+     * 菜单名称
+     */
+    private String name;
+    /**
+     * 菜单URL
+     */
+    private String url;
+    /**
+     * 授权标识(多个用逗号分隔，如：sys:menu:list,sys:menu:save)
+     */
+    private String authority;
+    /**
+     * 类型   0：菜单   1：按钮   2：接口
+     */
+    private Integer type;
+    /**
+     * 打开方式   0：内部   1：外部
+     */
+    private Integer openStyle;
+    /**
+     * 菜单图标
+     */
+    private String icon;
+    /**
+     * 排序
+     */
+    private Integer sort;
 
 }
