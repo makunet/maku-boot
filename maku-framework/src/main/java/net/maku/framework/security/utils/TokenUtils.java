adf61375c7106ef29cd78c592e8981456c59f137
阿沐 <babamu@126.com>
2024-02-24 12:22:15 +0800

    更新

--- maku-framework/src/main/java/net/maku/framework/security/utils/TokenUtils.java
+++ maku-framework/src/main/java/net/maku/framework/security/utils/TokenUtils.java
@@ -3,6 +3,7 @@ package net.maku.framework.security.utils;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.StrUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import net.maku.framework.common.utils.HttpContextUtils;
 
 /**
  * Token 工具类
@@ -17,6 +18,18 @@ public class TokenUtils {
      */
     public static String generator() {
         return UUID.fastUUID().toString(true);
+    }
+
+    /**
+     * 获取 AccessToken
+     */
+    public static String getAccessToken() {
+        HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
+        if (request == null) {
+            return null;
+        }
+
+        return getAccessToken(request);
     }
 
     /**
