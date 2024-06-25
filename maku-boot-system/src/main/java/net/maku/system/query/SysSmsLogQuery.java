fb0529a7fe9f17d7070e7d72f2f63c941af6fd87
阿沐 <babamu@126.com>
2024-06-25 17:18:35 +0800

    更新

--- maku-boot-system/src/main/java/net/maku/system/query/SysSmsLogQuery.java
+++ maku-boot-system/src/main/java/net/maku/system/query/SysSmsLogQuery.java
@@ -1,4 +1,4 @@
-package net.maku.message.query;
+package net.maku.system.query;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -6,16 +6,16 @@ import lombok.EqualsAndHashCode;
 import net.maku.framework.common.query.Query;
 
 /**
-* 短信日志查询
-*
-* @author 阿沐 babamu@126.com
-*/
+ * 短信日志查询
+ *
+ * @author 阿沐 babamu@126.com
+ */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Schema(description = "短信日志查询")
-public class SmsLogQuery extends Query {
-    @Schema(description = "平台ID")
-    private Long platformId;
+public class SysSmsLogQuery extends Query {
+    @Schema(description = "手机号")
+    private String mobile;
 
     @Schema(description = "平台类型")
     private Integer platform;
