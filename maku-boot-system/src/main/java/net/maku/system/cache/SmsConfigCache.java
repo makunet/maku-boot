ad3a8748bad6b7a33d21e5e9a20a9cbc0afc7416
阿沐 <babamu@126.com>
2024-06-25 17:23:04 +0800

    短信配置缓存

--- maku-boot-system/src/main/java/net/maku/system/cache/SmsConfigCache.java
+++ maku-boot-system/src/main/java/net/maku/system/cache/SmsConfigCache.java
@@ -1,8 +1,8 @@
-package net.maku.message.cache;
+package net.maku.system.cache;
 
 import lombok.AllArgsConstructor;
 import net.maku.framework.common.cache.RedisCache;
-import net.maku.message.sms.config.SmsConfig;
+import net.maku.sms.config.SmsConfig;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,24 +15,32 @@ import java.util.List;
  */
 @Service
 @AllArgsConstructor
-public class SmsPlatformCache {
+public class SmsConfigCache {
     private final RedisCache redisCache;
 
     /**
      * 短信平台轮询KEY
      */
-    private final String SMS_ROUND_KEY = "message:sms:round";
+    private final String SMS_ROUND_KEY = "sys:sms:round";
+    private final String SMS_ROUND_CODE_KEY = "sys:sms:round:code";
 
     /**
      * 短信平台列表KEY
      */
-    private final String SMS_PLATFORM_KEY = "message:sms:platform";
+    private final String SMS_PLATFORM_KEY = "sys:sms:platform";
 
     /**
      * 获取短信轮询值
      */
     public Long getRoundValue() {
         return redisCache.increment(SMS_ROUND_KEY);
+    }
+
+    /**
+     * 获取短信编码轮询值
+     */
+    public Long getRoundCodeValue() {
+        return redisCache.increment(SMS_ROUND_CODE_KEY);
     }
 
     public List<SmsConfig> list() {
