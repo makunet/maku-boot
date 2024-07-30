cfd774ef2490a547eecb66f9b07dd6936bc977ec
阿沐 <babamu@126.com>
2024-07-30 10:50:24 +0800

    优化

--- maku-framework/src/main/java/net/maku/framework/common/excel/ExcelFinishCallBack.java
+++ maku-framework/src/main/java/net/maku/framework/common/excel/ExcelFinishCallBack.java
@@ -11,14 +11,6 @@ import java.util.List;
 public interface ExcelFinishCallBack<T> {
 
     /**
-     * 导出后置处理数据
-     * Do after all analysed.
-     *
-     * @param result the result
-     */
-    void doAfterAllAnalysed(List<T> result);
-
-    /**
      * Do save batch.
      *
      * @param result the result
